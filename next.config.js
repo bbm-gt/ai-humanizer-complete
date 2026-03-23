466f4435bf2ce1ec3c9a2216c6d40b2de0dfbd1c
bbm-gt <bbm552240147852@gmail.com>
2026-03-24 00:15:02 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export', // 开启静态导出
+  images: { unoptimized: true } // 静态模式必须关闭图片优化
 }
-
 module.exports = nextConfig
